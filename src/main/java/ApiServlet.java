994ee834cfcc3fda4afc52aa7a49a4e64d23ff0a
Litmmp <litmmp@163.com>
2016-11-20 23:33:40 +0800

    基本项目配置

---
+++ src/main/java/ApiServlet.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Litmmp and other contributors
+ * Released under the MIT license
+ *
+ * Author: Litmmp
+ * Email: litmmp@163.com
+ */
+
+import javax.servlet.http.HttpServlet;
+
+public class ApiServlet extends HttpServlet {
+    
+}
